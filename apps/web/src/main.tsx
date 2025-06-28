832d09920da1acdbb74197140fd8bf6f9edfe781
cskiva <sebastianlgrant@gmail.com>
2025-06-28 20:02:50 +1000

    slight fixes

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -10,7 +10,6 @@ import Layout from "./components/Layout";
 import Lobby from "./Lobby";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { SimpleVideo } from "./components/SimpleVideo/SimpleVideo";
 import { SocketProvider } from "./_context/SocketContext";
 import WatchPage from "./components/Watch";
 
@@ -59,10 +58,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                   <WatchPage />
                 </Layout>
               }
-            />
-            <Route
-              path="/simple-video"
-              element={<SimpleVideo roomId="t" className="" isLocal />}
             />
           </Routes>
         </BrowserRouter>
